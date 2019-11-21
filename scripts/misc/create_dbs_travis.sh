94165d8b4e99ff3dc417e6d83a50ad329cde1754
Tobias <>
2019-11-22 08:57:26 +1300

    CI Test

--- scripts/misc/create_dbs_travis.sh
+++ scripts/misc/create_dbs_travis.sh
@@ -11,22 +11,22 @@ if [ "$BACKEND" = "geonode.geoserver" ]; then
 		"before_script")
 			echo "Setting up PostGIS Backend"
 			export GEONODE_PROJECT_PATH=$TRAVIS_BUILD_DIR
-      sudo -u postgres dropdb template_postgis
-      sudo -u postgres dropdb geonode
-      sudo -u postgres dropdb geonode_data
-      sudo -u postgres dropdb upload_test
-      sudo -u postgres dropdb test_upload_test
-      sudo -u postgres dropuser geonode
-      sudo -u postgres createuser geonode -d -s
-      sudo -u postgres psql -c "ALTER USER geonode WITH PASSWORD 'geonode';"
-      sudo -u postgres createdb template_postgis
-      sudo -u postgres psql -d template_postgis -c 'CREATE EXTENSION postgis;'
-      sudo -u postgres psql -d template_postgis -c 'GRANT ALL ON geometry_columns TO PUBLIC;'
-      sudo -u postgres psql -d template_postgis -c 'GRANT ALL ON spatial_ref_sys TO PUBLIC;'
-      sudo -u postgres psql -d template_postgis -c 'GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO geonode;'
-      sudo -u postgres createdb -O geonode geonode
-      sudo -u postgres createdb -T template_postgis -O geonode geonode_data
-      sudo -u postgres createdb -T template_postgis -O geonode upload_test
+      sudo -u postgres dropdb -h 127.0.0.1 -u runner template_postgis
+      sudo -u postgres dropdb -h 127.0.0.1 -u runner geonode
+      sudo -u postgres dropdb -h 127.0.0.1 -u runner geonode_data
+      sudo -u postgres dropdb -h 127.0.0.1 -u runner upload_test
+      sudo -u postgres dropdb -h 127.0.0.1 -u runner test_upload_test
+      sudo -u postgres dropuser -h 127.0.0.1 -u runner geonode
+      sudo -u postgres createuser -h 127.0.0.1 -u runner geonode -d -s
+      sudo -u postgres psql --h 127.0.0.1 -u runner c "ALTER USER geonode WITH PASSWORD 'geonode';"
+      sudo -u postgres createdb -h 127.0.0.1 -u runner template_postgis
+      sudo -u postgres psql --h 127.0.0.1 -u runner d template_postgis -c 'CREATE EXTENSION postgis;'
+      sudo -u postgres psql --h 127.0.0.1 -u runner d template_postgis -c 'GRANT ALL ON geometry_columns TO PUBLIC;'
+      sudo -u postgres psql --h 127.0.0.1 -u runner d template_postgis -c 'GRANT ALL ON spatial_ref_sys TO PUBLIC;'
+      sudo -u postgres psql --h 127.0.0.1 -u runner d template_postgis -c 'GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO geonode;'
+      sudo -u postgres createdb --h 127.0.0.1 -u runner O geonode geonode
+      sudo -u postgres createdb --h 127.0.0.1 -u runner T template_postgis -O geonode geonode_data
+      sudo -u postgres createdb --h 127.0.0.1 -u runner T template_postgis -O geonode upload_test
 			;;
 		"after_script")
 			;;
