7f463dcb06221eecdab4207dccc861a9d986da90
Tobias <>
2019-11-22 09:07:22 +1300

    CI Test

--- scripts/misc/create_dbs_travis.sh
+++ scripts/misc/create_dbs_travis.sh
@@ -11,16 +11,16 @@ if [ "$BACKEND" = "geonode.geoserver" ]; then
 		"before_script")
 			echo "Setting up PostGIS Backend"
 			export GEONODE_PROJECT_PATH=$TRAVIS_BUILD_DIR
-      createuser -h 127.0.0.1 -U runner geonode -d -s
-      psql -h 127.0.0.1 -U runner c "ALTER USER geonode WITH PASSWORD 'geonode';"
-      createdb -h 127.0.0.1 -U runner template_postgis
-      psql -h 127.0.0.1 -U runner d template_postgis -c 'CREATE EXTENSION postgis;'
-      psql -h 127.0.0.1 -U runner d template_postgis -c 'GRANT ALL ON geometry_columns TO PUBLIC;'
-      psql -h 127.0.0.1 -U runner d template_postgis -c 'GRANT ALL ON spatial_ref_sys TO PUBLIC;'
-      psql -h 127.0.0.1 -U runner d template_postgis -c 'GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO geonode;'
-      createdb -h 127.0.0.1 -U runner O geonode geonode
-      createdb -h 127.0.0.1 -U runner T template_postgis -O geonode geonode_data
-      createdb -h 127.0.0.1 -U runner T template_postgis -O geonode upload_test
+      createuser -h "127.0.0.1" -U runner geonode -d -s
+      psql -h "127.0.0.1" -U runner c "ALTER USER geonode WITH PASSWORD 'geonode';"
+      createdb -h "127.0.0.1" -U runner template_postgis
+      psql -h "127.0.0.1" -U runner d template_postgis -c 'CREATE EXTENSION postgis;'
+      psql -h "127.0.0.1" -U runner d template_postgis -c 'GRANT ALL ON geometry_columns TO PUBLIC;'
+      psql -h "127.0.0.1" -U runner d template_postgis -c 'GRANT ALL ON spatial_ref_sys TO PUBLIC;'
+      psql -h "127.0.0.1" -U runner d template_postgis -c 'GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO geonode;'
+      createdb -h "127.0.0.1" -U runner O geonode geonode
+      createdb -h "127.0.0.1" -U runner T template_postgis -O geonode geonode_data
+      createdb -h "127.0.0.1" -U runner T template_postgis -O geonode upload_test
 			;;
 		"after_script")
 			;;
