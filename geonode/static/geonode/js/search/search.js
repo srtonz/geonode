fc90407c64477fb00903575c9462fd2fbcb6ad93
Dana Lambert <danalambert@catalyst.net.nz>
2019-12-02 18:50:46 +1300

    Updating search script autocomplete url

--- geonode/static/geonode/js/search/search.js
+++ geonode/static/geonode/js/search/search.js
@@ -584,26 +584,8 @@
       }
     }
 
-    /*
-    * Text search management
-    */
-    var text_autocomplete = $('#text_search_input').yourlabsAutocomplete({
-          url: AUTOCOMPLETE_URL_RESOURCEBASE,
-          choiceSelector: 'span',
-          hideAfter: 200,
-          minimumCharacters: 1,
-          placeholder: gettext('Enter your text here ...'),
-          autoHilightFirst: false
-    });
 
-    $('#text_search_input').keypress(function(e) {
-      if(e.which == 13) {
-        $('#text_search_btn').click();
-        $('.yourlabs-autocomplete').hide();
-      }
-    });
-
-    $('#text_search_input').bind('selectChoice', function(e, choice, text_autocomplete) {
+    $('#text_search_input').bind('selectChoice', function(e, choice) {
           if(choice[0].children[0] == undefined) {
               $('#text_search_input').val($(choice[0]).text());
               $('#text_search_btn').click();
@@ -611,31 +593,22 @@
     });
 
     $('#text_search_btn').click(function(){
-        if (HAYSTACK_SEARCH)
+        if (HAYSTACK_SEARCH) {
             $scope.query['q'] = $('#text_search_input').val();
-        else
-            if (AUTOCOMPLETE_URL_RESOURCEBASE == "/autocomplete/ProfileAutocomplete/")
+        } else {
+            if (AUTOCOMPLETE_URL_RESOURCEBASE == "/people/autocomplete/") {
                 // a user profile has no title; if search was triggered from
                 // the /people page, filter by username instead
                 var query_key = 'username__icontains';
-            else
+            } else {
                 console.log('search key', $('#text_search_input').data());
                 var query_key = $('#text_search_input').data('query-key')||'title__icontains';
+            }
             $scope.query[query_key] = $('#text_search_input').val();
         query_api($scope.query);
+        }
     });
 
-    /*
-    * Region search management
-    */
-    var region_autocomplete = $('#region_search_input').yourlabsAutocomplete({
-          url: AUTOCOMPLETE_URL_REGION,
-          choiceSelector: 'span',
-          hideAfter: 200,
-          minimumCharacters: 1,
-          appendAutocomplete: $('#region_search_input'),
-          placeholder: gettext('Enter your region here ...')
-    });
     $('#region_search_input').bind('selectChoice', function(e, choice, region_autocomplete) {
           if(choice[0].children[0] == undefined) {
               $('#region_search_input').val(choice[0].innerHTML);
